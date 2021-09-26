4592261283e9298e575d2c573161739eb8425e37
XGalilei <ianbush1@gmail.com>
2021-09-26 16:24:14 -0500

    Adding in Express routes

--- server/server.js
+++ server/server.js
@@ -8,8 +8,32 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('build'));
 
-/** ---------- EXPRESS ROUTES ---------- **/
+const router = express.Router();
+const pool = require('../modules/pool');
 
+/** ---------- EXPRESS ROUTES ---------- **/
+router.get('/feedback', (req, res) => {
+    let queryText = 'SELECT * FROM "feedback" ORDER BY "date";';
+    pool.query(queryText).then(result => {
+        res.send(result.rows);
+    }).catch(error => {
+        console.log('Error in /GET:', error);
+        res.sendStatus(500);
+    });
+});
+
+router.post('/feedback', (req, res) => {
+    let newEntry = req.body;
+    let queryText = `INSERT INTO "feedback" 
+    ("feeling", "understanding", "support", "comments")
+    VALUES ($1 $2 $3 $4);`;
+    pool.query(queryText).then(result => {
+        res.sendStatus(201);
+    }).catch(error => {
+        console.log('Error in /POST:', error);
+        res.sendStatus(500);
+    });
+});
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
